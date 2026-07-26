06ddbd450dcfb9522a084557bbf8136fbfaaf872
federiosv <Federico.RiosVolentini@alu.frt.utn.edu.ar>
2026-07-26 17:44:30 -0300

    feat(disponibilidades): agregar entidad y configuración

---
+++ Dsw2026Tpi.Data/Configurations/AvailabilityConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Dsw2026Tpi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Dsw2026Tpi.Data.Configurations
+{
+    public class AvailabilityConfiguration : IEntityTypeConfiguration<Availability>
+    {
+        public void Configure(EntityTypeBuilder<Availability> builder)
+        {
+            builder.ToTable("Availabilities");
+
+            builder.Property(availability => availability.DoctorId).IsRequired();
+
+            builder.Property(availability => availability.Date).HasColumnType("date").IsRequired();
+
+            builder.Property(availability => availability.StartTime).HasColumnType("time(0)").IsRequired();
+
+            builder.Property(availability => availability.EndTime).HasColumnType("time(0)").IsRequired();
+
+            builder.Property(availability => availability.Status).HasConversion<string>().HasMaxLength(20).IsRequired();
+
+            builder.Property(availability => availability.RowVersion).IsRowVersion();
+
+            builder.HasOne<Doctor>().WithMany().HasForeignKey(availability => availability.DoctorId).OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasIndex(availability => new
+                {
+                    availability.DoctorId,
+                    availability.Date,
+                    availability.StartTime
+                }).IsUnique().HasFilter("[Deleted] = 0");
+        }
+
+
+    }
+}
---
+++ Dsw2026Tpi.Domain/Entities/Availability.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Tpi.Domain.Entities
+{
+    public class Availability : EntityBase
+    {
+        public Guid DoctorId { get; private set; }
+        public DateOnly Date { get; private set; }
+        public TimeOnly StartTime { get; private set; }
+        public TimeOnly EndTime { get; private set; }
+        public AvailabilityStatus Status { get; private set; }
+        public byte[] RowVersion { get; private set; } = [];
+
+        #region Constructor for EF
+    #pragma warning disable CS8618
+        private Availability()
+        {
+        }
+    #pragma warning restore CS8618
+        #endregion
+        public Availability(Guid doctorId, DateOnly date, TimeOnly startTime, Guid? id = null) : base(id)
+        {
+            if (DoctorId == Guid.Empty)
+                throw new ArgumentException("DoctorId cannot be empty.", nameof(doctorId));
+
+            DoctorId = doctorId;
+            Date = date;
+            StartTime = startTime;
+            EndTime = startTime.AddMinutes(30);
+            Status = AvailabilityStatus.Available;
+        }
+
+        public void Book()
+        {
+            if (Status != AvailabilityStatus.Available)
+            {
+                throw new InvalidOperationException("Solo se puede reservar un espacio Disponible.");
+            }
+
+            Status = AvailabilityStatus.Booked;
+        }
+        public void Release()
+        {
+            if (Status != AvailabilityStatus.Booked)
+            {
+                throw new InvalidOperationException("Solo se puede liberar un espacio Reservado");
+            }
+
+            Status = AvailabilityStatus.Available;
+        }
+        public void Block()
+        {
+            if (Status == AvailabilityStatus.Booked)
+            {
+                throw new InvalidOperationException("Un espacio reservado no puede ser Bloqueado");
+            }
+
+            Status = AvailabilityStatus.Blocked;
+        }
+    }
+}
