660cd6f37b77938c45b85cff40b890307f69fa1c
Fausto <fausto44190084@gmail.com>
2026-08-02 21:13:37 -0300

    fix(api): agregar prefijo api a rutas de especialidades

--- Dsw2026Tpi.Api/Controllers/SpecialityController.cs
+++ Dsw2026Tpi.Api/Controllers/SpecialityController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Dsw2026Tpi.Api.Controllers;
 
-[Route("specialties")]
+[Route("api/specialties")]
 [Authorize(Policy = Policies.AdminPolicy)]
 public class SpecialityController : AppController
 {
